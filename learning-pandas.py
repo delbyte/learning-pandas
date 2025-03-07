3dd28c8609073e3e880f9301ef19816c1bd7cc4e
delbyte <blazed0utcode@gmail.com>
2025-03-07 23:10:04 +0530

    dataframe and df made

--- learning-pandas.py
+++ learning-pandas.py
@@ -6,3 +6,8 @@ import matplotlib.pyplot as plt
 s = pd.Series([1,3,5,np.nan,6,8])
 print(s)
 
+#Creating a DataFrame by passing a numpy array, with a datetime index and labeled columns:
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.randn(6,4), index=dates, columns = list("ABCD"))
+print(dates)
+print(df)

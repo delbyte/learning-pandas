32f4431d131cc7161e214fad88dae27b2cb00f3c
delbyte <blazed0utcode@gmail.com>
2025-03-09 15:59:02 +0530

    added querying specific data

--- learning-pandas.py
+++ learning-pandas.py
@@ -2,16 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dates = list(pd.date_range(20250308, periods = 10))
+dates = list(pd.date_range("20250308", periods = 10))
 covidcases = ["Covid-19", "Omicron", "Omega"]
 df = pd.DataFrame(np.random.randn(10, 3), index = dates, columns = covidcases)
 
-print(df)
-print(df.head(1))
-print(df.tail(1))
-print(df.T)
-print(df.describe())
+print(f"the numbers for Omicron are \n{df['Omicron']}")
 
-print(df.sort_index(axis = 1, ascending=True))
+print(f"The first three rows of data-\n{df[0:3]}")
 
-print(f"the numbers for Omicron are \n {df["Omicron"]}")
+print(f"Querying data from specific dates-\n{df['20250310':'20250317']}")

37fee0744c4c02a4f157adf49d3703f8f1909e20
delbyte <blazed0utcode@gmail.com>
2025-03-11 21:29:52 +0530

    plot an interactive map and display it

--- learning-geopandas.py
+++ learning-geopandas.py
@@ -4,13 +4,19 @@ import geopandas as gpd
 import geodatasets 
 from geodatasets import get_path
 import matplotlib.pyplot as plt
-
+from shapely.geometry import Point
 
 path_to_data = get_path("absaustralia_states_territories")
 gdf = gpd.read_file(path_to_data)
 
+gdf = gdf.rename(columns={c: str(c) for c in gdf.columns})
+
 print(gdf.info())
 print("\n\n")
 print(gdf)
-gdf.plot()
-plt.show()
+m = gdf.explore()
+
+outfp = r"base_map.html"
+m.save(outfp)
+import webbrowser
+webbrowser.open(outfp)

f86900bcff16ee9a2661241b716d637fba759924
delbyte <blazed0utcode@gmail.com>
2025-03-11 21:20:34 +0530

    showing the plot

--- learning-geopandas.py
+++ learning-geopandas.py
@@ -3,6 +3,8 @@ import numpy as np
 import geopandas as gpd
 import geodatasets 
 from geodatasets import get_path
+import matplotlib.pyplot as plt
+
 
 path_to_data = get_path("absaustralia_states_territories")
 gdf = gpd.read_file(path_to_data)
@@ -10,3 +12,5 @@ gdf = gpd.read_file(path_to_data)
 print(gdf.info())
 print("\n\n")
 print(gdf)
+print(gdf.plot())
+plt.show()
